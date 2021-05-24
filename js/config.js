a71b72e0455ad5cf0b8cc72408666ea27154641e
Ioannis Apostolopoulos <IoApo@users.noreply.github.com>
2021-05-24 21:08:47 +0300

    initial upload and configuration

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+    'm788224588-f0383be6ae8f589eced6f211',
+    'm788224621-191819f7d053857ad96912d9',
+    'm788224597-ce05988e126a6fc0e6fded81',
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
